abb2b68be39f2d45becce24b7e5c2807a79509f9
Hamza Waseem <hamza-mirza@live.com>
2021-01-04 14:19:14 +0500

    Implementation of Login Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+//import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <h3 className="p-2 mt-4">Login</h3>
+            <div className="card mt-4">
+              <div className="card-body p-4">
+                <h3 className="card-title text-dark">
+                  Sign in to your account
+                </h3>
+                <div className="mt-4 form-group">
+                  <label className="text-dark d-block" htmlFor="email">
+                    <b>Email</b>
+                  </label>
+                  <input id="email" type="text" className="form-control" />
+                </div>
+                <div className="mt-4 form-group">
+                  <label className="text-dark" htmlFor="password">
+                    <b>Password</b>
+                  </label>
+                  <span className="text-dark float-right">
+                    <a href="/#">Forgot your password?</a>
+                  </span>
+                  <input
+                    id="password"
+                    type="password"
+                    className="form-control"
+                  />
+                </div>
+                <div className="mt-4 form-group">
+                  <input id="checkbox" type="checkbox" />
+                  <label className="text-dark ml-1" htmlFor="checkbox">
+                    <b>Stay signed in for a week</b>
+                  </label>
+                </div>
+                <div className="mt-4 form-group">
+                  <button
+                    style={{ width: "100%" }}
+                    className="btn btn-primary form-group"
+                  >
+                    Continue
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
